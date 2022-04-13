0cff78360f9d2007da7ac66df41e515d53703c41
EastLin <a3162858@gmail.com>
2022-04-14 02:34:17 +0800

    updata deleteTodos

--- deleteTodo.js
+++ deleteTodo.js
@@ -1,20 +1,17 @@
+const Todos = require("./model/todo");
 const successHandle = require("./successHandle");
 const errorHandle = require("./errorHandle");
 
-function deleteTodo(res, todos, req = null) {
+async function deleteTodo(res, todos, req = null) {
   if (!req) {
     todos.length = 0;
     successHandle(res, todos);
   } else {
     try {
       const id = req.url?.split("/")?.pop();
-      const index = todos.findIndex((element) => element.id === id);
-      if (index === -1) {
-        errorHandle(res);
-      } else {
-        todos.splice(index, 1);
-        successHandle(res, todos);
-      }
+      const result = await Todos.findByIdAndDelete(id);
+
+      successHandle(res, result);
     } catch (error) {
       errorHandle(res);
     }
