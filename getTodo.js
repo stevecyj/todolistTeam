4d220d36d977911916d01064649888972e18533c
stevecyj <ycsteven.tsao@gmail.com>
2022-04-15 03:22:32 +0800

    getTodo via mongoose

--- getTodo.js
+++ getTodo.js
@@ -1,7 +1,14 @@
+const Todos = require("./model/todo");
 const successHandle = require("./successHandle");
+const errorHandle = require("./errorHandle");
 
-function getTodo(res, todos) {
-  successHandle(res, todos);
+async function getTodo(req, res) {
+  try {
+    const todos = await Todos.find();
+    successHandle(res, todos);
+  } catch (error) {
+    errorHandle(res);
+  }
 }
 
 module.exports = getTodo;
